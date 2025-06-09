5b19cee3c3aa1e0adcad229a10ccb932f6e6d248
Victoria Cardoso <vvjcerque@hotmail.com>
2025-06-09 17:16:00 -0300

    Feat: add lista suspensa component

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,6 +1,16 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
   return (
     <section className="formulario">
       <form action="">
@@ -8,6 +18,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
       </form>
     </section>
   );
