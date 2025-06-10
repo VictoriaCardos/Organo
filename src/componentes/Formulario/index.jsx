bf2216d2537238a03bae37a7556da0692b0e4e35
Victoria Cardoso <vvjcerque@hotmail.com>
2025-06-10 16:02:14 -0300

    feat: add botao and prop obrigatorio

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,6 +1,7 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 const Formulario = () => {
   const times = [
     "Programação",
@@ -11,14 +12,27 @@ const Formulario = () => {
     "Mobile",
     "Inovação e Gestão",
   ];
+  // Função para lidar com o envio do formulário
+  const aoSalvar = (evento) => {
+    evento.preventDefault(); // Evita o recarregamento da página
+  };
   return (
     <section className="formulario">
-      <form action="">
+      <form action="" onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+        />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
         <ListaSuspensa label="Time" itens={times} />
+        <Botao> Criar Card </Botao>
       </form>
     </section>
   );
