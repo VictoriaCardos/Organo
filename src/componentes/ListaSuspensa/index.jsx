9f93b2dab1b94aa42a6a82e1dc714a63171359a4
Victoria Cardoso <vvjcerque@hotmail.com>
2025-06-11 15:17:06 -0300

    feat: add prop onChange

--- src/componentes/ListaSuspensa/index.jsx
+++ src/componentes/ListaSuspensa/index.jsx
@@ -3,7 +3,10 @@ const ListaSuspensa = (props) => {
   return (
     <div className="lista-suspensa">
       <label htmlFor="">{props.label}</label>
-      <select required={props.required}>
+      <select
+        onChange={(evento) => props.aoAlterado(evento.target.value)}
+        required={props.required}
+      >
         {props.itens.map((item) => (
           <option key={item}>{item}</option>
         ))}
