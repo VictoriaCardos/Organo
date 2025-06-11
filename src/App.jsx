f71bf62ea166a2e5fb8494c4645b8bfed155e4a7
Victoria Cardoso <vvjcerque@hotmail.com>
2025-06-11 15:39:19 -0300

    feat: add state to save form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner/index";
 import Formulario from "./componentes/Formulario";
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+  const aoNovoColaboradorCadastrado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador]);
+  };
   return (
     <div>
       <Banner />
-      <Formulario />
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorCadastrado(colaborador)
+        }
+      />
     </div>
   );
 }
