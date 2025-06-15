53e0ff4b38c2dba86edb885f4df59f8d4d591970
VictoriaCardos <vvjcerque@hotmail.com>
2025-06-15 12:58:01 -0300

    feat: add new prop and option on ListaSuspensa

--- src/componentes/ListaSuspensa/index.jsx
+++ src/componentes/ListaSuspensa/index.jsx
@@ -6,7 +6,11 @@ const ListaSuspensa = (props) => {
       <select
         onChange={(evento) => props.aoAlterado(evento.target.value)}
         required={props.required}
+        value={props.valor}
       >
+        <option value="" disabled>
+          Selecione o {props.label.toLowerCase()}
+        </option>
         {props.itens.map((item) => (
           <option key={item}>{item}</option>
         ))}
