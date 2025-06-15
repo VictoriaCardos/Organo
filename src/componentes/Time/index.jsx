2a6478df1205c6c0e945576e443157ecf393a987
VictoriaCardos <vvjcerque@hotmail.com>
2025-06-14 23:49:21 -0300

    feat: add Colaborador component

--- src/componentes/Time/index.jsx
+++ src/componentes/Time/index.jsx
@@ -1,9 +1,15 @@
 import "./Time.css";
+import Colaborador from "../Colaborador";
 
 const Time = (props) => {
   return (
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+      {props.colaboradores.map((colaborador) => (
+        <Colaborador key={colaborador.nome} nome={colaborador.nome} imagem={colaborador.imagem} cargo={colaborador.cargo} />
+      ))}
+      </div>
     </section>
   );
 };
