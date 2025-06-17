3468dfbf8450b6c98ba01bbedd3587cc0c76ed41
Victoria Cardoso <vvjcerque@hotmail.com>
2025-06-17 15:42:09 -0300

    feat: add props to Colaborador component

--- src/componentes/Time/index.jsx
+++ src/componentes/Time/index.jsx
@@ -2,15 +2,25 @@ import "./Time.css";
 import Colaborador from "../Colaborador";
 
 const Time = (props) => {
-  return (
-    (props.colaboradores.length >0) ? <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="colaboradores">
-      {props.colaboradores.map((colaborador) => (
-        <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} imagem={colaborador.imagem} cargo={colaborador.cargo} />
-      ))}
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            corSecundaria={props.corSecundaria}
+            corDeFundo={props.corPrimaria}
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            imagem={colaborador.imagem}
+            cargo={colaborador.cargo}
+            aoRemover={props.aoRemover}
+          />
+        ))}
       </div>
-    </section> : <></>
+    </section>
+  ) : (
+    <></>
   );
 };
 
