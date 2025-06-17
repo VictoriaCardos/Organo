4fad60c38a03170255f246c072ba153790ac3fad
Victoria Cardoso <vvjcerque@hotmail.com>
2025-06-17 15:45:55 -0300

    feat: add props for function and style

--- src/componentes/Botao/index.jsx
+++ src/componentes/Botao/index.jsx
@@ -1,6 +1,15 @@
 import "./Botao.css";
 const Botao = (props) => {
-  return <button className="botao">{props.children}</button>;
+  return (
+    <button
+      className={`botao ${props.className || ""}`}
+      onClick={props.onClick}
+      style={props.style}
+      type={props.type}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Botao;
