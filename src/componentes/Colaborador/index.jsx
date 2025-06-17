2eacc8b598386353a1dcceca40b2c12777aaa553
Victoria Cardoso <vvjcerque@hotmail.com>
2025-06-17 15:43:36 -0300

    feat: add props to remove and styles on Botao component

--- src/componentes/Colaborador/index.jsx
+++ src/componentes/Colaborador/index.jsx
@@ -1,18 +1,26 @@
-import './Colaborador.css';
+import "./Colaborador.css";
+import Botao from "../Botao";
 
 const Colaborador = (props) => {
   return (
     <div className="colaborador">
       <div className="cabecalho" style={{ backgroundColor: props.corDeFundo }}>
-        <img
-          src={props.imagem}
-          alt={`Imagem de ${props.nome}`}
-        />
+        <Botao
+          className="botao-excluir"
+          onClick={() => props.aoRemover(props.nome)}
+          style={{
+            backgroundColor: props.corSecundaria,
+            color: props.corDeFundo,
+          }}
+        >
+          Excluir
+        </Botao>
+        <img src={props.imagem} alt={`Imagem de ${props.nome}`} />
       </div>
-        <div className="rodape">
-            <h4>{props.nome}</h4>
-            <h5>{props.cargo}</h5>
-        </div>
+      <div className="rodape">
+        <h4>{props.nome}</h4>
+        <h5>{props.cargo}</h5>
+      </div>
     </div>
   );
 };
